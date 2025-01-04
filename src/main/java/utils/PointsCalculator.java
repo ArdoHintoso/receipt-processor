610de30cd3130578ac38dea56ecc07a391f226b1
ArdoHintoso <awilliams.hintoso@gmail.com>
2025-01-04 14:25:32 -0800

    utils completed and refactored with helpers with return type

--- src/main/java/utils/PointsCalculator.java
+++ src/main/java/utils/PointsCalculator.java
@@ -2,46 +2,98 @@ package utils;
 
 import models.Receipt;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 public class PointsCalculator {
-    int sumOfPoints;
-
     public int calculatePoints(Receipt receipt) {
-        getPointsFromRetailer(receipt.retailer());
-        getPointsFromTotal(Integer.parseInt(receipt.total()));
-        getPointsFromListOfItems(receipt.items());
+        int sumOfPoints = 0;
+
+        sumOfPoints += getPointsFromRetailer(receipt.retailer()); // for RULE #1
+        sumOfPoints += getPointsFromTotal(Double.parseDouble(receipt.total())); // for RULES #2 and 3
+        sumOfPoints += getPointsFromListOfItems(receipt.items()); // for RULES #4 and 5
+        sumOfPoints += getPointsFromDateOfPurchase(receipt.purchaseDate()); // for RULE #7
+        sumOfPoints += getPointsFromTimeOfPurchase(receipt.purchaseTime()); // for RULE #8
 
         return sumOfPoints;
     }
 
-    private void getPointsFromRetailer(String name) {
-        // RULE 1
+    private int getPointsFromRetailer(String name) {
+        if (name == null || name.length() == 0) return 0;
+
+        int pointsFromRetailerName = 0;
+
+        // RULE #1: "One point for every alphanumeric character in the retailer name"
+        pointsFromRetailerName += name.replaceAll("\\s+", "").length();
+
+        return pointsFromRetailerName;
     }
 
-    private void getPointsFromTotal(int price) {
-        if (price < 0) return ;
+    private int getPointsFromTotal(double price) {
+        if (price < 0) return 0;
 
-        // RULE 2: "50 points if the total is a round dollar amount with no cents"
+        int pointsFromTotalPrice = 0;
 
-        if (price % 0.25 == 0) {
-            sumOfPoints += 25; // RULE 3: "25 points if the total is a multiple of 0.25"
+        // RULE #2: "50 points if the total is a round dollar amount with no cents"
+        if (price == Math.floor(price)) {
+            pointsFromTotalPrice += 50;
         }
+
+        // RULE #3: "25 points if the total is a multiple of 0.25"
+        if (price % 0.25 == 0) {
+            pointsFromTotalPrice += 25;
+        }
+
+        return pointsFromTotalPrice;
     }
 
-    private void getPointsFromListOfItems(List<Receipt.Item> items) {
-        if (items == null || items.size() == 0) return ;
+    private int getPointsFromListOfItems(List<Receipt.Item> items) {
+        if (items == null || items.size() == 0) return 0;
 
-        sumOfPoints += (items.size() / 2) * 5; // RULE 4: "5 points for every two items on the receipt"
+        int pointsFromItems = 0;
+
+        // RULE #4: "5 points for every two items on the receipt"
+        pointsFromItems += (items.size() / 2) * 5;
 
         StringMethods strMethods = new StringMethods();
 
+        // RULE #5: "If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned."
         for (Receipt.Item item: items) {
             int lenOfCurrentItem = strMethods.trimAndReturnLength(item.shortDescription());
             if (lenOfCurrentItem % 3 == 0) {
-                sumOfPoints += (int) Math.ceil(Double.parseDouble(item.price()) * 0.2); // RULE 5
+                pointsFromItems += (int) Math.ceil(Double.parseDouble(item.price()) * 0.2);
             }
         }
 
+        return pointsFromItems;
+    }
+
+    private int getPointsFromDateOfPurchase(LocalDate date) {
+        if (date == null) return 0;
+
+        int pointsFromDate = 0;
+
+        // RULE #7: "6 points if the day in the purchase date is odd"
+        if (date.getDayOfMonth() % 2 == 1) {
+            pointsFromDate += 6;
+        }
+
+        return pointsFromDate;
+    }
+
+    private int getPointsFromTimeOfPurchase(LocalTime time) {
+        if (time == null) return 0;
+
+        int pointsFromTime = 0;
+
+        // RULE #8: "10 points if the time of purchase is after 2:00pm and before 4:00pm"
+        LocalTime start = LocalTime.of(14, 0), end = LocalTime.of(16,0);
+
+        if (time.isAfter(start) && time.isBefore(end)) {
+            pointsFromTime += 10;
+        }
+
+        return pointsFromTime;
     }
 }
--- src/main/java/utils/StringMethods.java
+++ src/main/java/utils/StringMethods.java
@@ -2,7 +2,28 @@ package utils;
 
 class StringMethods {
     public int trimAndReturnLength(String input) {
-        int slow = 1;
-        return slow;
+        if (input == null || input.length() == 0) return 0;
+
+        int slow = 0;
+
+        char[] arr = input.toCharArray();
+
+        for (int fast = slow; fast < arr.length; fast++) {
+            if (slow == 0 && arr[fast] == ' ') {
+                fast++;
+            } else if (slow == 0) {
+                arr[slow++] = arr[fast];
+            } else if (arr[fast] == ' ') {
+                if (arr[slow - 1] != ' ') {
+                    arr[slow++] = arr[fast];
+                } else {
+                    fast++;
+                }
+            } else {
+                arr[slow++] = arr[fast];
+            }
+        }
+
+        return slow - 1;
     }
 }
